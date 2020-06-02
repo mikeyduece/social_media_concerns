7a9f6b90d07ebce2ed6b4b0ecec8f623676213e2
Mike Heft <mikeheft@gmail.com>
2020-06-02 10:54:08 -0600

    Finalize likes

--- lib/social_media.rb
+++ lib/social_media.rb
@@ -3,9 +3,4 @@ require 'social_media/concerns/likeable'
 require 'social_media/services/likes/create'
 
 module SocialMedia
-  mattr_accessor :owner_class
-
-  def self.owner_class
-    @@owner_class.to_s.tilteize.constantize
-  end
 end
