b7720b70d85c914f6a57c5a9fb6c5592dc2e6e5c
Mike Heft <mikeheft@gmail.com>
2020-06-02 09:19:50 -0600

    Add to model

--- app/models/social_media/like.rb
+++ app/models/social_media/like.rb
@@ -1,4 +1,35 @@
 module SocialMedia
   class Like < ApplicationRecord
+    belongs_to :target, polymorphic: true
+    belongs_to :owner, polymorphic: true
+
+    after_commit :increment_number_of_likes, on: :create
+    before_destroy :decrement_number_of_likes
+
+    private
+
+    def increment_number_of_likes
+      raise_error_if_requried
+
+      update_like_count(:+)
+    end
+
+    def decrement_number_of_likes
+      raise_error_if_requried
+
+      update_like_count(:-)
+    end
+
+    def update_like_count(operator)
+      like_count = target.number_of_likes.send(operator, 1)
+
+      target.update(number_of_likes: like_count)
+    end
+
+    private
+
+    def raise_error_if_requried
+      raise NotImplementedError.new('You must add a #number_of_likes column to the model you wish to "Like"') unless target.respond_to?(:number_of_likes)
+    end
   end
 end
