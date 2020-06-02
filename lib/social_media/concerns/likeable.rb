3213a600ccdd8b1e6a0658dda8e6ec2ad6039a52
Mike Heft <mikeheft@gmail.com>
2020-06-02 09:34:37 -0600

    Refactor class_name option in Likeable

--- lib/social_media/concerns/likeable.rb
+++ lib/social_media/concerns/likeable.rb
@@ -4,8 +4,8 @@ module SocialMedia
       extend ActiveSupport::Concern
 
       included do
-        has_many :likes, class_name: 'Like', as: :target, dependent: :destroy
-        has_many :likeable_objects, class_name: 'Like', as: :owner, dependent: :destroy
+        has_many :likes, class_name: 'SocialMedia::Like', as: :target, dependent: :destroy
+        has_many :likeable_objects, class_name: 'SocialMedia::Like', as: :owner, dependent: :destroy
       end
 
       class_methods do
@@ -13,7 +13,7 @@ module SocialMedia
           attributes = [attributes] unless attributes.is_a?(Array)
 
           attributes.each do |attribute|
-            class_eval "has_many :by_liking_#{attribute}, -> { order(created_at: :desc) }, class_name: 'Like', source: :owner, source_type: '#{attribute.to_s.classify}', foreign_key: :owner_id, dependent: :destroy"
+            class_eval "has_many :by_liking_#{attribute}, -> { order(created_at: :desc) }, class_name: 'SocialMedia::Like', source: :owner, source_type: '#{attribute.to_s.classify}', foreign_key: :owner_id, dependent: :destroy"
             class_eval "has_many :liked_#{attribute}, through: :by_liking_#{attribute}, source: :target, source_type: '#{attribute.to_s.classify}'"
           end
         end
@@ -22,7 +22,7 @@ module SocialMedia
           attributes = [attributes] unless attributes.is_a?(Array)
 
           attributes.each do |attribute|
-            class_eval "has_many :by_liked_#{attribute}, -> { order(created_at: :desc) }, class_name: 'Like', source: :target, source_type: '#{attribute.to_s.classify}', foreign_key: :target_id, dependent: :destroy"
+            class_eval "has_many :by_liked_#{attribute}, -> { order(created_at: :desc) }, class_name: 'SocialMedia::Like', source: :target, source_type: '#{attribute.to_s.classify}', foreign_key: :target_id, dependent: :destroy"
             class_eval "has_many :liked_by_#{attribute}, through: :by_liked_#{attribute}, source: :owner, source_type: '#{attribute.to_s.classify}'"
           end
         end
