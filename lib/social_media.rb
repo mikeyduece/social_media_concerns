f5091113b148eafbe51fce05d88ce8c379ec93bc
Mike Heft <mikeheft@gmail.com>
2020-06-04 15:55:15 -0600

    Add requires to main social media

--- lib/social_media.rb
+++ lib/social_media.rb
@@ -1,6 +1,10 @@
 require 'social_media/engine'
 require 'social_media/concerns/likeable'
 require 'social_media/services/likes/create'
+require 'social_media/services/likes/destroy'
+require 'social_media/concerns/followable'
+require 'social_media/services/follows/create'
+require 'social_media/services/follows/destroy'
 
 module SocialMedia
 end
